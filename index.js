b166b1360491e99c1559f7f50ed2ec5a0cb21dc0
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-06-12 16:27:15 +0300

    full update

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const form = document.getElementById('form');
 const inputs = form.querySelectorAll('input');
+const ratingCard = document.getElementById('rating-card');
+const thankYouCard = document.getElementById('thank-you-card');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     inputs.forEach((input) => {
-        console.log(input.checked);
-    })
-})
+        if (input.checked) {
+            document.getElementById('score').innerHTML = input.value;
+        }
+    });
+
+    ratingCard.classList.add('hide');
+    thankYouCard.classList.remove('hide');
+});
